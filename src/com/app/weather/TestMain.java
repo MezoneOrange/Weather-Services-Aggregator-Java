8997274720b2d93b1814a26d0645b9594c73bfe7
MezoneOrange <mezor07@mail.ru>
2020-08-19 17:12:22 +0500

    Add checks

--- src/com/app/weather/TestMain.java
+++ src/com/app/weather/TestMain.java
@@ -1,7 +1,41 @@
 package com.app.weather;
 
+import com.app.weather.weatherApi.*;
+
 public class TestMain {
     public static void main(String[] args) {
 
+
     }
+
+    public static void checkOpenWeatherApi() {
+        WeatherApi map = new OpenWeatherMap();
+        map.getResponceCode();
+        map.getWeatherForecast("Moscow");
+    }
+
+    public static void checkYandexWeatherApi() {
+        WeatherApi map = new YandexWeatherApi();
+        map.getResponceCode();
+        map.getWeatherForecast("Moscow");
+    }
+
+    public static void checkGismeteoWeatherApi() {
+        WeatherApi map = new GismeteoWeatherApi();
+        map.getResponceCode();
+        map.getWeatherForecast("Moscow");
+    }
+
+    public static void checkWeatherStackApi() {
+        WeatherApi map = new WeatherStackApi();
+        map.getResponceCode();
+        map.getWeatherForecast("Moscow");
+    }
+
+    public static void checkWeatherForecast() {
+        WeatherForecast forecast = new WeatherForecast();
+        forecast.getListOfServices();
+        forecast.getForecast();
+    }
+
 }
