09e7064f999c1ada940dd409164c6c8a33c98bec
MezoneOrange <mezor07@mail.ru>
2020-08-20 11:00:17 +0500

    upgrated test cases

--- src/com/app/weather/TestMain.java
+++ src/com/app/weather/TestMain.java
@@ -10,31 +10,91 @@ public class TestMain {
     public static void main(String[] args) {
         List<String> cities = new ArrayList<>(Arrays.asList("Москва", "санкт петербург",
                                                             "miami", "ревда", "екат", "new york"));
-        cities.forEach(TestMain::checkWeatherForecast);
+        cities.forEach(TestMain::checkWeatherBit);
     }
 
     public static void checkOpenWeatherApi(String city) {
         WeatherApi map = new OpenWeatherMap();
-        System.out.println(map.getResponseCode());
-        System.out.println(map.getWeatherForecast(city));
+        ForecastObject obj = map.getResponseCode();
+
+        System.out.println(obj.getRESOURCE());
+        System.out.println(obj.getRESPONSE());
+        System.out.println(obj.getCITY());
+        System.out.println(obj.getTEMPERATURE());
+        System.out.println(obj.getWEATHER());
+        System.out.println(obj);
+
+        ForecastObject objCity = map.getWeatherForecast(city);
+
+        System.out.println(objCity.getRESOURCE());
+        System.out.println(objCity.getRESPONSE());
+        System.out.println(objCity.getCITY());
+        System.out.println(objCity.getTEMPERATURE());
+        System.out.println(objCity.getWEATHER());
+        System.out.println(objCity);
     }
 
     public static void checkWeatherStackApi(String city) {
         WeatherApi map = new WeatherStackApi();
-        System.out.println(map.getResponseCode());
-        System.out.println(map.getWeatherForecast(city));
+        ForecastObject obj = map.getResponseCode();
+
+        System.out.println(obj.getRESOURCE());
+        System.out.println(obj.getRESPONSE());
+        System.out.println(obj.getCITY());
+        System.out.println(obj.getTEMPERATURE());
+        System.out.println(obj.getWEATHER());
+        System.out.println(obj);
+
+        ForecastObject objCity = map.getWeatherForecast(city);
+
+        System.out.println(objCity.getRESOURCE());
+        System.out.println(objCity.getRESPONSE());
+        System.out.println(objCity.getCITY());
+        System.out.println(objCity.getTEMPERATURE());
+        System.out.println(objCity.getWEATHER());
+        System.out.println(objCity);
     }
 
     public static void checkWeatherCom(String city) {
         WeatherApi map = new WeatherCom();
-        System.out.println(map.getResponseCode());
-        System.out.println(map.getWeatherForecast(city));
+        ForecastObject obj = map.getResponseCode();
+
+        System.out.println(obj.getRESOURCE());
+        System.out.println(obj.getRESPONSE());
+        System.out.println(obj.getCITY());
+        System.out.println(obj.getTEMPERATURE());
+        System.out.println(obj.getWEATHER());
+        System.out.println(obj);
+
+        ForecastObject objCity = map.getWeatherForecast(city);
+
+        System.out.println(objCity.getRESOURCE());
+        System.out.println(objCity.getRESPONSE());
+        System.out.println(objCity.getCITY());
+        System.out.println(objCity.getTEMPERATURE());
+        System.out.println(objCity.getWEATHER());
+        System.out.println(objCity);
     }
 
     public static void checkWeatherBit(String city) {
         WeatherApi map = new WeatherBit();
-        System.out.println(map.getResponseCode());
-        System.out.println(map.getWeatherForecast(city));
+        ForecastObject obj = map.getResponseCode();
+
+        System.out.println(obj.getRESOURCE());
+        System.out.println(obj.getRESPONSE());
+        System.out.println(obj.getCITY());
+        System.out.println(obj.getTEMPERATURE());
+        System.out.println(obj.getWEATHER());
+        System.out.println(obj);
+
+        ForecastObject objCity = map.getWeatherForecast(city);
+
+        System.out.println(objCity.getRESOURCE());
+        System.out.println(objCity.getRESPONSE());
+        System.out.println(objCity.getCITY());
+        System.out.println(objCity.getTEMPERATURE());
+        System.out.println(objCity.getWEATHER());
+        System.out.println(objCity);
     }
 
     public static void checkWeatherForecast(String city) {
